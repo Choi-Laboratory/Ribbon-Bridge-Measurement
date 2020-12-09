8b51cf5ce250ee6ece8f791f9b0d3b15b12be72c
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2020-12-09 14:47:10 +0900

    update plotter.py

--- src/measured_data_plotter.py
+++ src/measured_data_plotter.py
@@ -10,7 +10,7 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-from jsk_recognition_msgs.msg import BoundingBox, BoundingBoxArray
+from arcode_ros.msg import MarkerInfo, MarkersInfo
 from ribbon_bridge_measurement.msg import RibbonBridge, RibbonBridges
 
 PACKAGE_PATH = rospkg.RosPack().get_path("ribbon_bridge_measurement") + "/"
@@ -21,7 +21,7 @@ class MeasuredDataPlotter():
 
         self.ribbon_bridge_num = 7
 
-        self.marker_subscriber = rospy.Subscriber("/arcode_ros/detect_result", BoundingBoxArray, self.marker_subscriber_callback)
+        self.marker_subscriber = rospy.Subscriber("/arcode_ros/detect_result", MarkersInfo, self.marker_subscriber_callback)
         self.measured_data_subscriber = rospy.Subscriber("/ribbon_bridge_measurement/measure_result", RibbonBridges, self.measured_data_subscriber_callback)
         self.img_subscriber = rospy.Subscriber("/usb_cam/image_raw", Image, self.img_subscriber_callback)
 
@@ -29,15 +29,15 @@ class MeasuredDataPlotter():
         self.img_height = -1
         self.is_get_img_size = False
 
-        self.markers_data = []
+        self.data_list = []
 
     def marker_subscriber_callback(self, msg):
-        bounding_boxes = [BoundingBox()]*self.ribbon_bridge_num
+        markers_info = [MarkerInfo()]*self.ribbon_bridge_num
         
-        for bounding_box in msg.boxes:
-            bounding_boxes[bounding_box.label] = bounding_box
+        for marker_info in msg.markers_info:
+            markers_info[marker_info.value] = marker_info
         
-        self.markers_data.append(bounding_boxes)
+        self.data_list.append(markers_info)
     
     def measured_data_subscriber_callback(self, msg):
         pass
@@ -56,19 +56,36 @@ class MeasuredDataPlotter():
                 rospy.logerr(e)
 
     def create_plot_img(self):
-        markers_data = self.markers_data
+        data_list = self.data_list
 
         white_img = np.ones((self.img_height, self.img_width, 3),np.uint8)*255
         
-        for marker_data in markers_data:
-            for i, data in enumerate(marker_data):
+        for markers_data in data_list:
+            for i, marker_data in enumerate(markers_data):
                 cv2.circle(white_img, \
-                        (int(data.pose.position.x), int(data.pose.position.y)),\
+                        (int(marker_data.center.x), int(marker_data.center.y)),\
                         radius=3, \
                         color=(0,i*20,255-i*20), \
                         thickness=-1, \
                         lineType=cv2.LINE_8, \
                         shift=0)
+                
+                cv2.line(white_img, \
+                        (int(marker_data.center.x), int(marker_data.center.y)),\
+                        (int((marker_data.right_top.x+marker_data.right_bottom.x)/2), int((marker_data.right_top.y+marker_data.right_bottom.y)/2)),\
+                        color=(0,0,255),\
+                        thickness=3,\
+                        lineType=cv2.LINE_8,\
+                        shift=0)
+                
+                cv2.line(white_img, \
+                        (int(marker_data.center.x), int(marker_data.center.y)),\
+                        (int((marker_data.right_top.x+marker_data.left_top.x)/2), int((marker_data.right_top.y+marker_data.left_top.y)/2)),\
+                        color=(255,0,0),\
+                        thickness=3,\
+                        lineType=cv2.LINE_8,\
+                        shift=0)
+
 
         cv2.imshow("Marker plot", white_img)
         cv2.waitKey(1) 
