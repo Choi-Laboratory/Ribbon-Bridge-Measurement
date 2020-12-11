63fef8965d45cc8048a78a5a0315dcd1c804bda3
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2020-12-11 21:16:19 +0900

    update file

--- src/measured_data_plotter.py
+++ src/measured_data_plotter.py
@@ -6,6 +6,7 @@
 import rospy, rospkg
 import cv2
 import numpy as np
+import math
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -29,7 +30,8 @@ class MeasuredDataPlotter():
         self.img_height = -1
         self.is_get_img_size = False
 
-        self.data_list = []
+        self.markers_info_list = []
+        self.measure_data_list = []
 
     def marker_subscriber_callback(self, msg):
         markers_info = [MarkerInfo()]*self.ribbon_bridge_num
@@ -37,11 +39,13 @@ class MeasuredDataPlotter():
         for marker_info in msg.markers_info:
             markers_info[marker_info.value] = marker_info
         
-        self.data_list.append(markers_info)
+        self.markers_info_list.append(markers_info)
     
     def measured_data_subscriber_callback(self, msg):
-        pass
-    
+        self.measure_data_list.append(msg.RibbonBridges)
+
+        
+
     def img_subscriber_callback(self, msg):
         if self.is_get_img_size == True:
             pass
@@ -56,35 +60,61 @@ class MeasuredDataPlotter():
                 rospy.logerr(e)
 
     def create_plot_img(self):
-        data_list = self.data_list
+        data_list = self.markers_info_list
 
         white_img = np.ones((self.img_height, self.img_width, 3),np.uint8)*255
         
-        for markers_data in data_list:
-            for i, marker_data in enumerate(markers_data):
-                cv2.circle(white_img, \
-                        (int(marker_data.center.x), int(marker_data.center.y)),\
-                        radius=3, \
-                        color=(0,i*20,255-i*20), \
-                        thickness=-1, \
-                        lineType=cv2.LINE_8, \
-                        shift=0)
-                
-                cv2.line(white_img, \
-                        (int(marker_data.center.x), int(marker_data.center.y)),\
-                        (int((marker_data.right_top.x+marker_data.right_bottom.x)/2), int((marker_data.right_top.y+marker_data.right_bottom.y)/2)),\
-                        color=(0,0,255),\
-                        thickness=3,\
-                        lineType=cv2.LINE_8,\
-                        shift=0)
-                
-                cv2.line(white_img, \
-                        (int(marker_data.center.x), int(marker_data.center.y)),\
-                        (int((marker_data.right_top.x+marker_data.left_top.x)/2), int((marker_data.right_top.y+marker_data.left_top.y)/2)),\
-                        color=(255,0,0),\
-                        thickness=3,\
-                        lineType=cv2.LINE_8,\
-                        shift=0)
+        for i, markers_data in enumerate(data_list):
+            for id, marker_data in enumerate(markers_data):
+                if marker_data != MarkerInfo(): #マーカの検出に成功した場合
+                    #マーカーの傾きを計算
+                    radian = math.atan2(marker_data.right_top.y - marker_data.left_top.y, marker_data.right_top.x - marker_data.left_top.x)
+
+                    #中心点の描画
+                    cv2.circle(white_img, \
+                            (int(marker_data.center.x), int(marker_data.center.y)),\
+                            radius=1, \
+                            color=(0,id*20,255-id*20), \
+                            thickness=-1, \
+                            lineType=cv2.LINE_8, \
+                            shift=0)
+                    
+                    #一つ前のフレームのマーカーと現在のマーカーを結ぶ線を描画
+                    if i != 0:
+                        counter = 1
+                        while True:
+                            last_marker_data = data_list[i-counter][id]
+                            if last_marker_data != MarkerInfo():
+                                break
+                            else:
+                                counter += 1
+
+                        cv2.arrowedLine(white_img, \
+                                (int(last_marker_data.center.x), int(last_marker_data.center.y)),\
+                                (int(marker_data.center.x), int(marker_data.center.y)),\
+                                color=(0,id*20,255-id*20),\
+                                thickness=1,\
+                                tipLength=0.25,\
+                                shift=0)
+                    
+                    
+                    #一番新しい点は、角度の軸も描画する
+                    if i == len(data_list)-1:
+                        cv2.line(white_img, \
+                                (int(marker_data.center.x), int(marker_data.center.y)),\
+                                (int((marker_data.right_top.x+marker_data.right_bottom.x)/2), int((marker_data.right_top.y+marker_data.right_bottom.y)/2)),\
+                                color=(0,0,255),\
+                                thickness=3,\
+                                lineType=cv2.LINE_8,\
+                                shift=0)
+                        
+                        cv2.line(white_img, \
+                                (int(marker_data.center.x), int(marker_data.center.y)),\
+                                (int((marker_data.right_top.x+marker_data.left_top.x)/2), int((marker_data.right_top.y+marker_data.left_top.y)/2)),\
+                                color=(255,0,0),\
+                                thickness=3,\
+                                lineType=cv2.LINE_8,\
+                                shift=0)
 
 
         cv2.imshow("Marker plot", white_img)
@@ -94,7 +124,8 @@ class MeasuredDataPlotter():
         while not rospy.is_shutdown():
             try:
                 if self.is_get_img_size == True:
-                    self.create_plot_img()
+                    #self.create_plot_img()
+                    pass
             
             except KeyboardInterrupt:
                 break
